cb6df3cd1be77efe6d06e78d4baac845b7150039
Artur <1008480224@ukr.net>
2021-06-16 13:08:51 +0300

    form

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,10 @@ import { applyMiddleware, combineReducers, createStore } from 'redux'
 
 import thunk from 'redux-thunk' //thunk middleware
 import countryListReducer from './countryListReducer'
-import { reducer as formReducer } from 'redux-form'
 
 const reducersPush = combineReducers({
   countiesCovidPart: countryListReducer,
-  form: formReducer,
+ 
 })
 
 export type globalStateType = ReturnType<typeof reducersPush>
